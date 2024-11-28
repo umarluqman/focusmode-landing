a826746dc6c20cf31da21758c8eaa3fd5c38e183
umarluqman <umarluqman.78@gmail.com>
2024-11-28 09:22:13 +0800

    added black friday promo

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -35,8 +35,8 @@ export default function ProVersionPage() {
     "Support Indie Developer",
   ];
   const { data: session } = useSession();
-  const [promoCode, setPromoCode] = useState("LAUNCH30");
-  const [value, setValue] = useState("LAUNCH30");
+  const [promoCode, setPromoCode] = useState("BLACKFRIDAY");
+  const [value, setValue] = useState("BLACKFRIDAY");
   const onSubmit = () => {
     setPromoCode(value);
   };
@@ -116,14 +116,14 @@ export default function ProVersionPage() {
             </CardHeader>
             <CardContent>
               <div className="text-4xl font-bold mb-6 flex items-center space-x-4">
-                <div>{promoCode ? "$20.99" : "$29.99"}</div>
+                <div>{promoCode ? "$10.99" : "$29.99"}</div>
                 {promoCode && (
                   <div className="flex flex-col">
                     <span className="text-base font-normal line-through text-zinc-400">
                       <s>$29.99</s>
                     </span>
                     <span className="text-xs font-semibold text-green-500">
-                      Save ${(29.99 - 20.99).toFixed(2)}
+                      Save ${(29.99 - 10.99).toFixed(2)}
                     </span>
                   </div>
                 )}
