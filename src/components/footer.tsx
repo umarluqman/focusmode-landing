da1cacf5846f677d570e9e35c1258b8e282c41b9
umarluqman <umarluqman.78@gmail.com>
2025-04-16 17:16:57 +0800

    fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export let Footer = () => {
   return (
     <footer className="flex flex-row justify-between w-full border-t bg-white px-4 py-3 md:px-6 dark:bg-gray-900 mt-auto">
       <p className="text-xs text-gray-500 dark:text-gray-400">
-        © 2024 focusmode. All rights reserved.
+        © {new Date().getFullYear()} focusmode. All rights reserved.
       </p>
       <p className="text-xs text-gray-500 dark:text-gray-400">
         <Link href="/privacy-policy">Privacy Policy</Link>
