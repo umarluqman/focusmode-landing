0d28bdb00fd3f5f7e54be77c655b0424087a14a7
umarluqman <umarluqman.78@gmail.com>
2025-11-26 08:26:08 +0800

    update bf sale

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -26,18 +26,18 @@ export default function ProVersionPage() {
   const [isHovered, setIsHovered] = useState(false);
 
   const features = [
-    "3 PRO interventions",
-    "Customizable interventions",
+    "Breathing intervention",
     "Ad-free experience",
     "Browsing analytics",
     "Muti-device Sync",
     "Lifetime updates",
     "Priority support",
     "Support Indie Developer",
+    "Advanced Customization (Coming Soon)",
   ];
   const { data: session } = useSession();
-  const [promoCode, setPromoCode] = useState("LAUNCH30");
-  const [value, setValue] = useState("LAUNCH30");
+  const [promoCode, setPromoCode] = useState("BF2025");
+  const [value, setValue] = useState("BF2025");
   const onSubmit = () => {
     setPromoCode(value);
   };
@@ -103,7 +103,7 @@ export default function ProVersionPage() {
           >
             <div className="flex flex-col gap-8 lg:w-1/2 items-center text-center">
               <h1 className="text-6xl font-bold text-zinc-900 bg-clip-text text-transparent leading-snug">
-                Buy Once, Use Forever
+                Black Friday Sale!
               </h1>
               <LandingSocialProof showRating avatarItems={avatarItems} />
             </div>
@@ -127,14 +127,14 @@ export default function ProVersionPage() {
               </CardHeader>
               <CardContent>
                 <div className="text-4xl font-bold mb-6 flex items-center space-x-4">
-                  <div>{promoCode ? "$20.99" : "$29.99"}</div>
+                  <div>{promoCode ? "$5.99" : "$29.99"}</div>
                   {promoCode && (
                     <div className="flex flex-col">
                       <span className="text-base font-normal line-through text-zinc-400">
                         <s>$29.99</s>
                       </span>
                       <span className="text-xs font-semibold text-green-500">
-                        Save ${(29.99 - 20.99).toFixed(2)}
+                        Save ${(29.99 - 5.99).toFixed(2)}
                       </span>
                     </div>
                   )}
